24653ad01204ad896379543897298e034045129a
yukiueno <yuukiueno1217@gmail.com>
2020-08-26 17:08:37 +0900

    授業情報横スライド完成　CSS JS　追加分

---
+++ nizi/public/js/top.js
@@ -0,0 +1,11 @@
+$(function(){
+$('.your-class').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 100000000000000,
+    arrows:true,
+  });
+
+
+          });
