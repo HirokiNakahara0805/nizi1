1a2b442d0be9fe69b8e76eb7bc5f99314e4292ae
yukiueno <yuukiueno1217@gmail.com>
2020-08-29 01:25:52 +0900

    掲示板画面デザイン　微調整

--- nizi/resources/views/bbs/index.blade.php
+++ nizi/resources/views/bbs/index.blade.php
@@ -29,146 +29,150 @@
     </div>
 @endif
 
-<div class="mt-4 mb-4">
-    @foreach($categories as $id => $name)
-    <span class="btn"><a href="{{ route('bbs.index', ['category_id'=>$id]) }}" title="{{ $name }}">{{ $name }}</a></span>
-    @endforeach
-</div>
 
-<div class="mt-4 mb-4">
-    <p>{{ $posts->total() }}件が見つかりました。</p>
-</div>
+<div class="all-index">
 
-<div class="container mt-4">
-    <div class="border p-2">
 
-        <form method="POST" action="{{ route('bbs.store') }}" >
-            @csrf
+    <div class="container mt-4">
+        <div class="border p-2">
 
-            <fieldset class="">
-                <div class="row text-center">
-                    <div class="form-group col-sm-6">
-                        
-                        <input
-                            id="name"
-                            name="name"
-                            class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
-                            value="{{ old('name') }}"
-                            type="text"
-                            placeholder="ニックネーム"
-                        >
-                        @if ($errors->has('name'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('name') }}
-                            </div>
-                        @endif
+            <form method="POST" action="{{ route('bbs.store') }}" >
+                @csrf
+
+                <fieldset class="">
+                    <div class="row text-center">
+                        <div class="form-group col-sm-6">
+                            
+                            <input
+                                id="name"
+                                name="name"
+                                class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
+                                value="{{ old('name') }}"
+                                type="text"
+                                placeholder="ニックネーム"
+                            >
+                            @if ($errors->has('name'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('name') }}
+                                </div>
+                            @endif
+                        </div>
+        
+                    
+                        <div class="form-group col-sm-6">
+                            
+                            <input
+                                id="subject"
+                                name="subject"
+                                class="form-control {{ $errors->has('subject') ? 'is-invalid' : '' }}"
+                                value="{{ old('subject') }}"
+                                type="text"
+                                placeholder="学年"
+                            >
+                            @if ($errors->has('subject'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('subject') }}
+                                </div>
+                            @endif
+                        </div>
+
                     </div>
     
-                
-                    <div class="form-group col-sm-6">
-                        
-                        <input
-                            id="subject"
-                            name="subject"
-                            class="form-control {{ $errors->has('subject') ? 'is-invalid' : '' }}"
-                            value="{{ old('subject') }}"
-                            type="text"
-                            placeholder="学年"
-                        >
-                        @if ($errors->has('subject'))
+                    <div class="form-group">
+                        <label for="message">
+                            メイン内容
+                        </label>
+    
+                        <textarea
+                            id="message"
+                            name="message"
+                            class="form-control {{ $errors->has('message') ? 'is-invalid' : '' }}"
+                            rows="1"
+                        >{{ old('message') }}</textarea>
+                        @if ($errors->has('message'))
                             <div class="invalid-feedback">
-                                {{ $errors->first('subject') }}
+                                {{ $errors->first('message') }}
                             </div>
                         @endif
                     </div>
 
-                </div>
- 
-                <div class="form-group">
-                    <label for="message">
-                        メイン内容
-                    </label>
- 
-                    <textarea
-                        id="message"
-                        name="message"
-                        class="form-control {{ $errors->has('message') ? 'is-invalid' : '' }}"
-                        rows="1"
-                    >{{ old('message') }}</textarea>
-                    @if ($errors->has('message'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('message') }}
-                        </div>
-                    @endif
-                </div>
-
-                <div class="form-group">
-                
-                <input
-                    id="category_id"
-                    name="category_id"
-                    value = "{{ $category_id }}"
-                    class="form-control {{ $errors->has('category_id') ? 'is-invalid' : '' }}"
-                    text="text"
-                    type="hidden"
-                >
-                </div>
- 
- 
-                <div class="mt-2 text-right">
- 
-                    <button type="submit" class="btn btn-primary">
-                        投稿する
-                    </button>
-                </div>
-            </fieldset>
-        </form>
+                    <div class="form-group">
+                    
+                    <input
+                        id="category_id"
+                        name="category_id"
+                        value = "{{ $category_id }}"
+                        class="form-control {{ $errors->has('category_id') ? 'is-invalid' : '' }}"
+                        text="text"
+                        type="hidden"
+                    >
+                    </div>
+    
+    
+                    <div class="mt-2 text-right">
+    
+                        <button type="submit" class="btn btn-primary">
+                            投稿する
+                        </button>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
     </div>
-</div>
+
+    <div class="mt-4 mb-4">
+        <p>{{ $posts->total() }}件が見つかりました。</p>
+    </div>
 
 
 
 
+    <div class="container">
+        <div class="table table-hover">
+            <!-- <thead>
+                <tr>
 
-</div>
-<div class="container">
-    <table class="table table-hover">
-        <!-- <thead>
-            <tr>
+                    <th>投稿時間</th>
+                    <th>ニックネーム</th>
+                    <th>学年</th>
+                    <th>メイン内容</th>
+                </tr>
+            </thead> -->
+            <div id="tbl">
+            @foreach ($posts as $post)
 
-                <th>投稿時間</th>
-                <th>ニックネーム</th>
-                <th>学年</th>
-                <th>メイン内容</th>
-            </tr>
-        </thead> -->
-        <tbody id="tbl">
-        @foreach ($posts as $post)
+                <div class="comment-box">
+                    <div class="comment-text">{!! nl2br(e(Str::limit($post->message, 100))) !!}
+                        @if ($post->comments->count() >= 1)
+                            <p><span class="badge badge-primary">コメント：{{ $post->comments->count() }}件</span></p>
+                        @endif
+                    </div>
+                    <div class="">
+                        <div class="f-container comment-info">
+                            <div class="f-item float-right">{{ $post->created_at->format('Y.m.d H:i') }}</div>
+                            <div class="f-item float-right">{{ $post->name }}</div>
+                            <div class="f-item float-right ">{{ $post->subject }}学年</div>
+                            <div class="f-item float-right"><a href="{{ action('PostsController@show', $post->id) }}" class="btn btn-primary btn-sm">reply</a></div>
 
-            <div class="comment-box">
-                <div class="comment-text">{!! nl2br(e(Str::limit($post->message, 100))) !!}
-                    @if ($post->comments->count() >= 1)
-                        <p><span class="badge badge-primary">コメント：{{ $post->comments->count() }}件</span></p>
-                    @endif
-                </div>
-                <div class="">
-                    <div class="f-container comment-info">
-                        <div class="f-item float-right">{{ $post->created_at->format('Y.m.d H:i') }}</div>
-                        <div class="f-item float-right">{{ $post->name }}</div>
-                        <div class="f-item float-right ">{{ $post->subject }}学年</div>
-                        <div class="f-item float-right"><a href="{{ action('PostsController@show', $post->id) }}" class="btn btn-primary btn-sm">reply</a></div>
-
-                        
+                            
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center mb-5">
+        {{ $posts->appends(['category_id' => $category_id])->links() }}
+    </div>
+    <h3 class="text-center">授業科目</h3>
+    <div class="mt-4 mb-4 text-center underlist">
+        @foreach($categories as $id => $name)
+        <li class="btn"><a class="class-list" href="{{ route('bbs.index', ['category_id'=>$id]) }}" title="{{ $name }}">{{ $name }}</a></span>
         @endforeach
-        </tbody>
-    </table>
-</div>
-<div class="d-flex justify-content-center mb-5">
-    {{ $posts->appends(['category_id' => $category_id])->links() }}
+    </div>
+
 </div>
 @endsection
 
