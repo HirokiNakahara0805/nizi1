36b8945d6c0c898769439c4528a4d1e73a9a4a6b
yukiueno <yuukiueno1217@gmail.com>
2020-09-01 20:28:31 +0900

    TOP ページ　デザイン　brushup

--- nizi/public/js/top.js
+++ nizi/public/js/top.js
@@ -1,6 +1,6 @@
 $(function(){
 $('.your-class').slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 100000000000000,
--- nizi/resources/views/parts/header.blade.php
+++ nizi/resources/views/parts/header.blade.php
@@ -1,14 +1,18 @@
 
 <link rel="stylesheet" href="{{ asset('/css/top.css') }}">
 
-<header class="header">
-	<div class="header-color">
-		<p class="header-logo">∞pilotis</p>
-		<nav class="nav">
-			<ul>
-				<li class="nav-item"><a href="#class-sub">Subject</a></li>
-				<li class="nav-item"><a href="#all-bbs">BBS</a></li>
-			</ul>
-		</nav>
+<header class="u2-header">
+	<div class="u2-header__container-inner">
+		<a href="/" ><p class="u2-header-logo">∞pilotis</p></a>
 	</div>
 </header>
+
+<div class="u2-global-navbar">
+	<ul class="reset-ul row v2-global-navbar__links">
+		<li class="u2-global-navbar__link"><a href="#class-sub">classc subject</a><i class="fas fa-angle-double-down nav-arrows"></i></li>
+		<li class="u2-global-navbar__link"><a href="#all-bbs">bbs</a><i class="fas fa-angle-double-down nav-arrows"></i></li>
+		<li class="u2-global-navbar__link"><a href="#all-bbs">people</a><i class="fas fa-angle-double-down nav-arrows"></i></li>
+		<li class="u2-global-navbar__link"><a href="#all-bbs">sns</a><i class="fas fa-angle-double-down nav-arrows"></i></li>
+		<li class="u2-global-navbar__link"><a href="#all-bbs">about </a><i class="fas fa-angle-double-down nav-arrows"></i></li>
+	</ul>
+</div>
