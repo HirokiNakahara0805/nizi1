e750c4553f0beb7be8ff21f9c173dea115afecca
yukiueno <yuukiueno1217@gmail.com>
2020-09-08 14:45:12 +0900

    ４学年に変更

--- nizi/database/factories/Post2Factory.php
+++ nizi/database/factories/Post2Factory.php
@@ -9,7 +9,7 @@ $factory->define(Post2::class, function (Faker $faker) {
     return [
         'created_at' => $faker->date('Y-m-d H:i:s', 'now'),
         'updated_at' => $faker->date('Y-m-d H:i:s', 'now'),
-        'year' => $faker->numberBetween(1,5),    // 1〜5のランダムな整数
+        'year' => $faker->numberBetween(1,4),    // 1〜5のランダムな整数
         'message' => $faker->realText(200),    // 200文字のテキスト
         'name' => $faker->name,    // 氏名
         'category_id' => $faker->numberBetween(1,5),    // 1〜5のランダムな整数
