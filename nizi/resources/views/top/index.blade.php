366155d3a3c742049dab72fb2a2f50fbe1781bd4
yukiueno <yuukiueno1217@gmail.com>
2020-09-12 00:59:53 +0900

    TOPページのコード整えました。

--- nizi/resources/views/top/index.blade.php
+++ nizi/resources/views/top/index.blade.php
@@ -6,128 +6,127 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-
-
 </head>
 
+	<!-- -------------------COMMONPARTS読み込み-->
 @include("parts.common")
+<!-- -------------------HEADER読み込み-->
 @include("parts.header")
 
 <body id="vw vh">
+
 	<!-- トップページ最初に開いた時のアニメーション-->
+	 <div class="start"><img src="{{ asset('img/8pilotis.1.jpg') }}" alt=""></div>
 
-   				 <div class="start"><img src="{{ asset('img/8pilotis.1.jpg') }}" alt=""></div>
+	<main>
 
-<main>
-
-	<!-- 画像挿入-->
-	<div class='header-img-frame'>
-		<div class='header-img-in'>
-			<img src="{{ asset('img/IMG_9584.JPG') }}" class="img-fluid h-100 w-100" >
-		</div>
-	</div>
-
-	<!-- 科目一覧表示 -->
-	<div class="to-class" id="class-sub">
-		<h1	class="text-center p-3 m-3" >Class subject</h1>
-
-		<div class="container text-center fluid">
-		 	<!-- 学科表示(スライダー) -->
-			<div class="row your-class">
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-						
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-						
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-						
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-					
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-
-				<div class=" img-sophia col-md-2" >
-						<a href="{{ url('/general') }}" >
-						
-						<h1 class="your-class">General education</h1>
-						</a>
-				</div>
-
+		<!-- 画像挿入-->
+		<div class='header-img-frame'>
+			<div class='header-img-in'>
+				<img src="{{ asset('img/IMG_9584.JPG') }}" class="img-fluid h-100 w-100" >
 			</div>
 		</div>
-	</div>
 
-	<!-- 掲示板一覧 -->
+		<!-- 科目一覧表示 -->
+		<div class="to-class" id="class-sub">
 
+			<h1	class="text-center p-3 m-3" >Class subject</h1>
 
+			<div class="container text-center fluid">
+				<!-- 学科表示(スライダー) -->
+				<div class="row your-class">
 
-
-		<div class="container" id="all-bbs">
-
-			<div class="row">
-
-				<div class="col-md-8 text-center top-all-bbs-backgroundcolor">
-
-					@foreach($category2s as $category2)
-					<div class="top-bbs-box">
-							<div class="border-bottom top-bbs-item-frame">
-									<div class="top-bbs-item "><p>{{ $category2->time }}</p></div>
-									<div class="top-bbs-item "><p>{{ $category2->teacher }}</p></div>
-							</div>
-						<a href="{{ route('generaltop.index', ['category_id'=>$category2->id ]) }}" title="{{ $category2->name }}">
-
-							<div class="">
-									<p class="bbs-name text-center" >{{ $category2->name }}</p>
-							</div>
-
-						</a>
+					<!-- 学科表示スライダーの中身-->
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
 					</div>
-					@endforeach
-				</div>
 
-				<div class="col-md-4">
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
-						<a><p class="koukoku-box">広告</p></a>
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
+					</div>
+
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
+					</div>
+
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
+					</div>
+
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
+					</div>
+
+					<div class=" img-sophia col-md-2" >
+							<a href="{{ url('/general') }}" >
+								<h1 class="your-class">General education</h1>
+							</a>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
+		<!-- 掲示板一覧 -->
+
+			<div class="container" id="all-bbs">
+
+				<div class="row">
+
+					<div class="col-md-8 text-center top-all-bbs-backgroundcolor">
+						<!-- カテゴリーテーブルを変数宣言してます -->
+						@foreach($category2s as $category2)
+						<div class="top-bbs-box">
+								<div class="border-bottom top-bbs-item-frame">
+										<div class="top-bbs-item "><p>{{ $category2->time }}</p></div>
+										<div class="top-bbs-item "><p>{{ $category2->teacher }}</p></div>
+								</div>
+							<a href="{{ route('generaltop.index', ['category_id'=>$category2->id ]) }}" title="{{ $category2->name }}">
+
+								<div class="">
+										<p class="bbs-name text-center" >{{ $category2->name }}</p>
+								</div>
+
+							</a>
+						</div>
+						@endforeach
+					</div>
+
+					<div class="col-md-4">
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+							<a><p class="koukoku-box">広告</p></a>
+					</div>
+
 				</div>
 
 			</div>
 
-		</div>
 
 
 
+	<!--------------------------------上にスクロール設定-->
+		<p id="page-top" ><a href="rolling"><i class="fas fa-arrow-up"></i></a></p>
+	</main>
 
 
-	<p id="page-top" ><a href="rolling"><i class="fas fa-arrow-up"></i></a></p>
-</main>
+<!--------------------------------フッター読み込み-->
 @include("parts.footer")
 
 		 <!--CSS (top用読み込)-->
@@ -140,5 +139,5 @@
 	
 </body>
 
-</html> 
+
 
