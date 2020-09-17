dcfe9ecf02ca14cb0be9e9c7ca336704f0f365d7
kokinakayama <kokibad0603@icloud.com>
2020-09-17 15:16:40 +0900

    フッター調整

--- nizi/resources/views/bbs/index.blade.php
+++ nizi/resources/views/bbs/index.blade.php
@@ -297,13 +297,12 @@
                 <p>
             　　    <a href="terms">利用規約</a>
                    <a href="beginnersguide">初めての方へ</a>
-                   <a href="http://127.0.0.1:8000/beginnersguide#beginners-organization">運営組織</a>
+                   <a href="beginnersguide">運営組織</a>
                    <a href="contentpolicy">コンテンツポリシー</a>
                    <a href="privacypolicy">プライバシーポリシー</a>
-                   <a href="http://127.0.0.1:8000/beginnersguide#beginners-faq">よくある質問</a>
-                   <a href="http://127.0.0.1:8000/beginnersguide#beginners-inquiry">お問い合わせ</a>
+                   <a href="beginnersguide">よくある質問</a>
+                   <a href="beginnersguide">お問い合わせ</a>
                 </p>
-
             </div>
             </div>
             
