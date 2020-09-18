c93c6ef35f13690e1084c27d82cc618423dea20a
yukiueno <yuukiueno1217@gmail.com>
2020-09-18 20:04:27 +0900

    フッターなど

--- nizi/resources/views/parts/footer.blade.php
+++ nizi/resources/views/parts/footer.blade.php
@@ -1,13 +1,13 @@
 
 <footer>
         <!------------------------------------------------------------------------------------ SNS連携 -->
-        <div class="footer-contents .align-middle" id="all-bbs">
+        <div class="footer-contents .a pgn-middle" id="all-bbs">
 
             <a class="footer-logo" href="{{ url('/') }}">∞piotis</a>
 
                <!-- SNSリスト-->
             <div class="footer-sns justify-content-around footer-logo" >
-                <div class="sns-lists">
+                <div class="sns- psts">
                      <!-- TWITTER　(fontawesomeでアイコン読み込み)-->
                     <div class="sns-item twitter list-inline-item">
                         <a target="_blank" href="https://mobile.twitter.com/8pilotis">
@@ -29,13 +29,17 @@
                 </div>
 
                 <div class="link-list">
-                    <div class="form-group col-sm-1"><a href="terms">利用規約</a></div>
-                    <div class="form-group col-sm-2"><a href="beginnersguide">初めての方へ</a></div>
-                    <div class="form-group col-sm-1"><a href="beginnersguide#organization">運営組織</a></div>
-                    <div class="form-group col-sm-2"><a href="contentpolicy">コンテンツポリシー</a></div>
-                    <div class="form-group col-sm-2"><a href="privacypolicy">プライバシーポリシー</a></div>
-                    <div class="form-group col-sm-2"><a href="beginnersguide#faq">よくある質問</a></div>
-                    <div class="form-group col-sm-2"><a href="beginnersguide#inquiry">お問い合わせ</a></div>
+                   <div class="text-center">
+                        <p>--------------------</p>
+                        <p class=""><a href="terms">利用規約</a></p>
+                        <p class=""><a href="beginnersguide">初めての方へ</a></p>
+                        <p class=""><a href="beginnersguide#organization">運営組織</a></p>
+                        <p class=""><a href="contentpolicy">コンテンツポリシー</a></p>
+                        <p class=""><a href="privacypolicy">プライバシーポリシー</a></p>
+                        <p class=""><a href="beginnersguide#faq">よくある質問</a></p>
+                        <p class=""><a href="beginnersguide#inquiry">お問い合わせ</a></p>
+                        <p>--------------------</p>
+                        </div>
                 </div>
             </div>
 
